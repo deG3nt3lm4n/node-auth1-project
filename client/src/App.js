80bb3dc433de4a70e364581d42962966e2620559
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-19 15:33:04 -0400

    debug + client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>what is life</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
